dd3d08b069eec9342131f1aa88a57f108e1be14e
Anoop Hallur <anooprh@thoughtworks.com>
2013-05-16 10:21:02 +0530

    Added Playing card deck implementation

---
+++ CardGame/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  CardGame
+//
+//  Created by anooprh on 16/05/13.
+//  Copyright (c) 2013 learn. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ CardGame/PlayingCardDeck.m
@@ -0,0 +1,30 @@
+//
+//  PlayingCardDeck.m
+//  CardGame
+//
+//  Created by anooprh on 16/05/13.
+//  Copyright (c) 2013 learn. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuites]) {
+            for (NSUInteger rank = 1 ; rank <= [PlayingCard maxRank] ; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                
+                [self addCard:card atTop:YES];
+            }
+        }
+    }
+    return self;
+}
+
+@end
