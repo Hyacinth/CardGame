1aab7fe9339d55978be877a1eb2169350f18e152
Anoop Hallur <anooprh@thoughtworks.com>
2013-05-17 14:51:05 +0530

    Some refactoring

--- CardGame/PlayingCard.m
+++ CardGame/PlayingCard.m
@@ -46,14 +46,16 @@
 
 - (int)match:(NSArray *)otherCards {
     int score = 0;
-    if ([otherCards count] == 1) {
-        PlayingCard *otherCard = [otherCards lastObject];
-        if ([otherCard.suit isEqualToString:self.suit]) {
+    id otherCard = [otherCards lastObject];
+    if ([otherCard isKindOfClass:[PlayingCard class]]) {
+        PlayingCard *otherPlayingCard = (PlayingCard *)otherCard;
+        if ([otherPlayingCard.suit isEqualToString:self.suit]) {
             score = 1;
         }
-        else if (otherCard.rank == self.rank){
-            score = 4;  
+        else if (otherPlayingCard.rank == self.rank){
+            score = 4;
         }
+
     }
     return score;
 }
